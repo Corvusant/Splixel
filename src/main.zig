1735a43fd01de391627187300846337013585728
Josua Kucher <josua.kucher@gmail.com>
2025-04-05 00:14:39 +0200

    Fixed all remaining comp errors

--- src/main.zig
+++ src/main.zig
@@ -7,93 +7,117 @@ const OptionalFuncs = @import("optional");
 const Optional = @import("optional").Optional;
 const Tuple = @import("optional").Tuple;
 
-const InputImages = struct {
-    Image1: std.fs.File,
-    Image2: std.fs.File,
-};
-
-const InputFolder = struct {
-    Folder: []const u8,
-};
+const InputImages = struct { Images: [2]std.fs.File };
 
 const TemplateFile = struct {
-    TemplateFile: std.fs.File,
+    File: std.fs.File,
 };
 
 const OutputFile = struct {
     File: []const u8,
 };
 
-const InputType = enum {
-    File,
-    Folder,
-};
-const InputUnion = union(InputType) { File: InputImages, Folder: InputFolder };
+pub fn TryOpenFileFromPath(path: []const u8, flags: std.fs.File.OpenFlags) Optional(std.fs.File) {
+    const file = std.fs.openFileAbsolute(path, flags) catch {
+        std.debug.print("File {s} cannot be opened. Check the Path.\n", .{path});
+        return Optional(std.fs.File).None();
+    };
 
-pub fn TryOpenFileFromPath(path: []const u8) Optional(std.fs.File) {
-    const file = std.fs.openFileAbsolute(path, .{});
-    if (file) {
-        return Optional{file};
-    } else {
-        std.debug.print("Template File {s} cannot be opened. Check the Path.\n", .{});
-        return Optional{};
-    }
+    return Optional(std.fs.File).Init(file);
 }
 
-pub fn ProcessInputArgs(args: [][]const u8) Optional(InputUnion) {
+pub fn TryCreateFileFromPath(path: []const u8) Optional(std.fs.File) {
+    const file = std.fs.createFileAbsolute(path, .{}) catch {
+        std.debug.print("File {s} cannot be Created. Check the Path.\n", .{path});
+        return Optional(std.fs.File).None();
+    };
+
+    return Optional(std.fs.File).Init(file);
+}
+
+pub fn IsPathFolderValid(path: []const u8) bool {
+    //Find the first / in the path
+    var sliceEndpoint = 0;
+    var i: usize = 0;
+    while (i > 0) : (i -= 1) {
+        if (path[i] == '/') {
+            sliceEndpoint = i;
+            break;
+        }
+    }
+    const folderpath = path[0..sliceEndpoint];
+    return if (std.fs.openDirAbsolute(folderpath, .{})) true else false;
+}
+
+pub fn ProcessInputArgs(allocator: std.mem.Allocator, args: [][:0]u8) Optional(InputImages) {
     const argumentNumber = args.len;
 
     for (args, 0..) |arg, i| {
-        if (i == 0) return null; //ignore the path to our own executable
+        if (i == 0) continue; //ignore the path to our own executable
         if (std.mem.eql(u8, arg, "-idir") and argumentNumber > i) {
-            return Optional(InputUnion){InputFolder{ .Folder = args[i + 1] }};
+            const pngFiles = FetchAllPNGFiles(allocator, args[i + 1]);
+
+            if (pngFiles.items.len < 2) {
+                std.debug.print("The provided Folder does not contain 2 Images to Diff\n", .{});
+                return Optional(InputImages).None();
+            }
+
+            if (pngFiles.items.len > 2) {
+                std.debug.print("The providerd folder contains more than 2 .png images. The first two will be used\n", .{});
+            }
+            return Optional(InputImages).Init(.{ .Images = [2]std.fs.File{ pngFiles.items[0], pngFiles.items[1] } });
         }
         if (std.mem.eql(u8, arg, "-ifile") and argumentNumber > i + 1) {
-            const file1 = TryOpenFileFromPath(args[i + 1]);
-            const file2 = TryOpenFileFromPath(args[i + 2]);
+            const file1 = TryOpenFileFromPath(args[i + 1], .{});
+            const file2 = TryOpenFileFromPath(args[i + 2], .{});
 
-            return OptionalFuncs.Zip(file1, file2)
-                .Bind(InputUnion, struct {
-                pub fn f(t: Tuple(std.fs.File, std.fs.File)) TemplateFile {
-                    return InputUnion{InputImages{ .Image1 = t.m1, .Image2 = t.m2 }};
+            return OptionalFuncs.Zip(std.fs.File, std.fs.File, file1, file2)
+                .Bind(InputImages, struct {
+                pub fn f(t: Tuple(std.fs.File, std.fs.File)) InputImages {
+                    return .{ .Images = [2]std.fs.File{ t.m1, t.m2 } };
                 }
             }.f);
         }
-        std.debug.print("{}: {s}\n", .{ i, arg });
     }
+    return Optional(InputImages).None();
 }
 
-pub fn ProcessOututArgs(args: [][]const u8) ?OutputFile {
+pub fn ProcessOututArgs(args: [][:0]u8) Optional(OutputFile) {
     const argumentNumber = args.len;
 
     for (args, 0..) |arg, i| {
-        if (i == 0) return null; //ignore the path to our own executable
+        if (i == 0) continue; //ignore the path to our own executable
         if (std.mem.eql(u8, arg, "-o") and argumentNumber > i) {
             std.debug.print("{}: {s}\n", .{ i, arg });
-            return OutputFile{
-                .File = args[i + 1],
-            };
+            const potentialFilePath = args[i + 1];
+            if (TryOpenFileFromPath(potentialFilePath, .{}).value) |_| {
+                return Optional(OutputFile).Init(.{ .File = potentialFilePath });
+            } else {
+                return Optional(OutputFile).None();
+            }
         }
     }
+    return Optional(OutputFile).None();
 }
 
-pub fn ProcessTemplateArgs(args: [][]const u8) Optional(TemplateFile) {
+pub fn ProcessTemplateArgs(args: [][:0]u8) Optional(TemplateFile) {
     const argumentNumber = args.len;
 
     for (args, 0..) |arg, i| {
-        if (i == 0) return null; //ignore the path to our own executable
+        if (i == 0) continue; //ignore the path to our own executable
         if (std.mem.eql(u8, arg, "-t") and argumentNumber > i) {
             std.debug.print("{}: {s}\n", .{ i, arg });
 
             const potentialFilePath = args[i + 1];
-            return TryOpenFileFromPath(potentialFilePath)
+            return TryOpenFileFromPath(potentialFilePath, .{})
                 .Bind(TemplateFile, struct {
                 pub fn f(file: std.fs.File) TemplateFile {
-                    return TemplateFile{ .file = file };
+                    return TemplateFile{ .File = file };
                 }
             }.f);
         }
     }
+    return Optional(TemplateFile).None();
 }
 
 pub fn IsValidFile(filename: []const u8) bool {
@@ -105,68 +129,106 @@ pub fn IsValidFile(filename: []const u8) bool {
     return false;
 }
 
-pub fn FetchAllPNGFiles(allocator: std.mem.Allocator, folderPath: []const u8) !std.ArrayList([]const u8) {
-    var filePaths = std.ArrayList([]const u8).init(allocator);
-    var dir = try std.fs.cwd().openDir(folderPath, .{ .iterate = true });
+pub fn FetchAllPNGFiles(allocator: std.mem.Allocator, folderPath: []const u8) std.ArrayList(std.fs.File) {
+    var filePaths = std.ArrayList(std.fs.File).init(allocator);
+    var dir = std.fs.cwd().openDir(folderPath, .{ .iterate = true }) catch {
+        return filePaths;
+    };
     defer dir.close();
 
-    var walker = try dir.walk(allocator);
+    var walker = dir.walk(allocator) catch {
+        return filePaths;
+    };
     defer walker.deinit();
 
-    while (try walker.next()) |item| {
+    while (walker.next() catch {
+        return filePaths;
+    }) |item| {
         if (item.kind != .file)
             continue;
 
         if (!IsValidFile(item.path))
             continue;
 
-        const pathName = try dir.realpathAlloc(allocator, "");
-        const fullImagePath = try std.fmt.allocPrint(allocator, "{s}\\{s}", .{ pathName, item.path });
-        try filePaths.append(fullImagePath);
+        const pathName = dir.realpathAlloc(allocator, "") catch {
+            return filePaths;
+        };
+        const fullImagePath = std.fmt.allocPrint(allocator, "{s}\\{s}", .{ pathName, item.path }) catch {
+            return filePaths;
+        };
+        if (TryOpenFileFromPath(fullImagePath, .{}).value) |f| {
+            filePaths.append(f) catch {
+                std.debug.print("Could not add files to process", .{});
+            };
+        }
     }
     return filePaths;
 }
 
 const Encoder = std.base64.standard.Encoder;
-pub fn ConvertImageToBas64(allocator: std.mem.Allocator, filepath: []const u8) ![]const u8 {
-    const path = filepath;
-    const file = try std.fs.openFileAbsolute(path, .{});
-    defer file.close();
+pub fn ConvertImageToBas64(allocator: std.mem.Allocator, file: std.fs.File) []const u8 {
+    const filesize = file.getEndPos() catch {
+        return "";
+    };
 
-    const filesize = try file.getEndPos();
-
-    const filecontent = try file.readToEndAlloc(allocator, filesize);
+    const filecontent = file.readToEndAlloc(allocator, filesize) catch {
+        return "";
+    };
     const encodedLength = Encoder.calcSize(filesize);
-    const encodedFile = try allocator.alloc(u8, encodedLength);
+    const encodedFile = allocator.alloc(u8, encodedLength) catch {
+        return "";
+    };
+
     return Encoder.encode(encodedFile, filecontent);
 }
 
-pub fn CreateHTMLPageWithImage(allocator: std.mem.Allocator, filePath: []const u8, encodedImage: []const u8) !void {
-    const path = filePath;
-    var file = try std.fs.cwd().createFile(path, .{});
-    const fileContent = try std.fmt.allocPrint(allocator, "<html><body><img src=\"data:image/png;base64, {s}\"></body></html>", .{encodedImage});
-    try file.writeAll(fileContent);
+pub fn CreateHTMLPage(allocator: std.mem.Allocator, outputfile: OutputFile, encodedImage1: []const u8, encodedImage2: []const u8) !void {
+    const fileContent = try std.fmt.allocPrint(allocator, "<html><body><img src=\"data:image/png;base64, {s}\"><img src=\"data:image/png;base64, {s}\"></body></html>", .{ encodedImage1, encodedImage2 });
+
+    if (TryCreateFileFromPath(outputfile.File).value) |file| {
+        try file.writeAll(fileContent);
+    } else {
+        if (TryOpenFileFromPath(outputfile.File, .{}).value) |file| {
+            try file.writeAll(fileContent);
+        }
+    }
+}
+
+pub fn CreateHTMLPageFromTemplate(allocator: std.mem.Allocator, outputFile: OutputFile, encodedImage1: []const u8, encodedImage2: []const u8) !void {
+    const fileContent = try std.fmt.allocPrint(allocator, "<html><body><img src=\"data:image/png;base64, {s}\"><img src=\"data:image/png;base64, {s}\"></body></html>", .{ encodedImage1, encodedImage2 });
+
+    if (TryCreateFileFromPath(outputFile.File).value) |file| {
+        try file.writeAll(fileContent);
+    } else {
+        if (TryOpenFileFromPath(outputFile.File, .{}).value) |file| {
+            try file.writeAll(fileContent);
+        }
+    }
 }
 
 pub fn main() !void {
     var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const gpa = general_purpose_allocator.allocator();
-    const args = std.process.argsAlloc(gpa);
+    const args = std.process.argsAlloc(gpa) catch {
+        std.debug.print("No Valid Aruments provided\n", .{});
+        return {};
+    };
     defer std.process.argsFree(gpa, args);
 
-    if (args) {
-        const input = ProcessInputArgs(args);
-        const output = ProcessOututArgs(args);
-        const template = ProcessTemplateArgs(args);
-    } else {
-        std.debug.print("No Valid Aruments provided\n");
-    }
-
-    const pngFiles = try FetchAllPNGFiles(gpa, inputPath);
-    defer pngFiles.deinit();
-    for (pngFiles.items) |file| {
-        std.debug.print(" File Found {s}", .{file});
-        const encodedImage = try ConvertImageToBas64(gpa, file);
-        try CreateHTMLPageWithImage(gpa, outputPath, encodedImage);
+    const input = ProcessInputArgs(gpa, args);
+    const output = ProcessOututArgs(args);
+    const template = ProcessTemplateArgs(args);
+    if (OptionalFuncs.Zip(InputImages, OutputFile, input, output).value) |requiredInputs| {
+        const encodedImage1 = ConvertImageToBas64(gpa, requiredInputs.m1.Images[0]);
+        const encodedImage2 = ConvertImageToBas64(gpa, requiredInputs.m1.Images[1]);
+        if (template.IsSet()) {
+            CreateHTMLPageFromTemplate(gpa, requiredInputs.m2, encodedImage1, encodedImage2) catch {
+                std.debug.print("Could not write to content to output File", .{});
+            };
+        } else {
+            CreateHTMLPage(gpa, requiredInputs.m2, encodedImage1, encodedImage2) catch {
+                std.debug.print("Could not write to content to output File", .{});
+            };
+        }
     }
 }
--- src/optional.zig
+++ src/optional.zig
@@ -1,21 +1,41 @@
 pub fn Optional(comptime Domain: type) type {
     return struct {
         value: ?Domain,
+        const Self = @This();
 
-        pub fn IsSet() bool {
-            return if (.value) {
+        pub fn None() Self {
+            return Self{ .value = null };
+        }
+
+        pub fn Init(v: Domain) Self {
+            return Self{ .value = v };
+        }
+
+        pub fn IsSet(self: Self) bool {
+            return if (self.value) |_| {
                 return true;
             } else {
                 return false;
             };
         }
 
-        pub fn Bind(comptime Codomain: type, comptime func: anytype) ?Optional(Codomain) {
-            if (.value) |value| {
-                return Optional(Codomain){func(value)};
+        pub fn Bind(self: Self, comptime Codomain: type, comptime func: anytype) Optional(Codomain) {
+            if (self.value) |value| {
+                return Optional(Codomain).Init(func(value));
             } else {
-                return null;
+                return Optional(Codomain).None();
             }
+        }
+
+        //Allows to run a function on the Optional content, cannot capture
+        pub fn Iter(comptime func: anytype) void {
+            if (.value) |value| {
+                func(value);
+            } else {}
+        }
+
+        pub fn Flatten(comptime Codomain: type, comptime setFunc: anytype, comptime unsetFunc: anytype) Codomain {
+            return if (.value) |value| setFunc(value) else unsetFunc();
         }
     };
 }
@@ -27,14 +47,14 @@ pub fn Tuple(comptime t1: type, comptime t2: type) type {
     };
 }
 
-pub fn Zip(comptime l: Optional, comptime r: Optional) Optional(Tuple) {
+pub fn Zip(comptime t1: type, comptime t2: type, l: Optional(t1), r: Optional(t2)) Optional(Tuple(t1, t2)) {
     if (l.value) |lvalue| {
         if (r.value) |rvalue| {
-            return Optional(Tuple){.{ .m1 = lvalue, .m2 = rvalue }};
+            return Optional(Tuple(t1, t2)).Init(.{ .m1 = lvalue, .m2 = rvalue });
         } else {
-            return Optional(Tuple){null};
+            return Optional(Tuple(t1, t2)).None();
         }
     } else {
-        return Optional(Tuple){null};
+        return Optional(Tuple(t1, t2)).None();
     }
 }
